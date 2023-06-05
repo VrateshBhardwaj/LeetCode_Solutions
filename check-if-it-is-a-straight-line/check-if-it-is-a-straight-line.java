d4f821c56a4f17f7f29b18283e9c8c513d335332
Vratesh Bhardwaj <102753634+CosmicWarriorCW@users.noreply.github.com>
2023-06-06 00:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (5.16%) - LeetSync

--- check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.java
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.java
@@ -2,24 +2,25 @@ class Solution
 {
     public boolean checkStraightLine(int[][] coordinates) 
     {
-			
+		double slope = (double)(coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
 		boolean lieOnLine = true;
 		
 		for(int i=2;i<coordinates.length;i++)
 		{
 
-			      int x1 = coordinates[0][0];
-            int y1 = coordinates[0][1];
-            int x2 = coordinates[1][0];
-            int y2 = coordinates[1][1];
-            int x  = coordinates[i][0];
-            int y  = coordinates[i][1];
+			  int x0 = coordinates[0][0];
+        int y0 = coordinates[0][1];
+        int x1 = coordinates[1][0];
+        int y1 = coordinates[1][1];
+				int x  = coordinates[i][0];
+        int y  = coordinates[i][1];
+
+			      double currentSlope = (double)(coordinates[i][1] - coordinates[0][1]) / (coordinates[i][0] - coordinates[0][0]);
 			
-				if((x2 - x1) * (y - y1) != (y2 - y1) * (x - x1))
-				{
-					lieOnLine = false;
-					break;
-				}
+			 if ((x - x0) * (y1 - y0) - (y - y0) * (x1 - x0) != 0) {
+                lieOnLine = false;
+                break;
+            }
 		   }
 			 return lieOnLine;
 		}
