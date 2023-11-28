45e4e45cdc8053842e62cf4a2a7db6c70836209f
Vratesh Bhardwaj <102753634+CosmicWarriorCW@users.noreply.github.com>
2023-11-28 22:31:03 +0530

    Time: 67 ms (37.39%) | Memory: 16.5 MB (35.28%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -8,27 +8,23 @@ class Solution(object):
         i = 0
         ch = ''
         vowel_index = []
-
+        vowels = ['a','e','i','o','u','A','E','I','O','U']
         for ch in s:
-            if ch.lower() in ['a', 'e', 'i', 'o', 'u']:
+            if ch in vowels:
                 vowel_index.append(index)
-                print(index)
             index += 1
         
-        vowel_index.reverse()
-        string_list = list(s)
-        print(string_list)
-
-        for ch in s:
-            if ch.lower() in ['a', 'e', 'i', 'o', 'u']:
-                temp = s[vowel_index[i]]
-                string_list[vowel_index[-1-i]] = string_list[vowel_index[i]]
-                string_list[vowel_index[-1-i]] = temp
-                i+=1
-        s = ''.join(string_list) 
-
-        print(string_list)
         
+        string_list = list(s)
+        
+        for ch in s:
+            if ch.lower() in vowels:
+                temp = s[vowel_index[-1-i]]
+                string_list[vowel_index[i]] = string_list[vowel_index[-1-i]]
+                string_list[vowel_index[i]] = temp
+                i+=1
+
+        s = ''.join(string_list) 
 
         return s
         
