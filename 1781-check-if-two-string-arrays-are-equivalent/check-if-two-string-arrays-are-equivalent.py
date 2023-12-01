b9bd7efb88a8521b8784d3bc0e58e92e582f0edd
Vratesh Bhardwaj <102753634+CosmicWarriorCW@users.noreply.github.com>
2023-12-01 13:35:07 +0530

    Time: 19 ms (28.16%) | Memory: 13.5 MB (25.77%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
@@ -5,9 +5,10 @@ class Solution(object):
         :type word2: List[str]
         :rtype: bool
         """
+        str_word1 = ''.join(word1)
+        str_word2 = ''.join(word2)
         
-        
-        return ''.join(word1) == ''.join(word2)
+        return str_word1 == str_word2
     
     
             
