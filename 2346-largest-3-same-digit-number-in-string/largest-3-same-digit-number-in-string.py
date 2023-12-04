82be6bc916fe431a68e590b20ffe896e190f3b69
Vratesh Bhardwaj <102753634+CosmicWarriorCW@users.noreply.github.com>
2023-12-04 10:45:27 +0530

    Time: 25 ms (23.08%) | Memory: 13.6 MB (6.41%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -3,15 +3,15 @@ class Solution(object):
        
         str = ''
         for i in range(0, len(num)-2, 1):
-            if num[i+2] == num[i+1] == num[i]:
+            if num[i+2] == num[i+1] and num[i+1] == num[i]:
                 str = 3*num[i]
                 break
-        #print(str)
-        for j in range(0, len(num)-2):
-            sbstr = num[j:j+3]
+            else:
+                str = ''
+        
+        if str:
+            for j in range(0, len(num)-2, 1):
+                 if (str[0] < num[j+2]) and num[j+2] == num[j+1] and num[j+1] == num[j]:
+                    str = 3*num[j]
 
-            if str and (str[0] < num[j+2]) and sbstr[0] == sbstr[1] == sbstr[2]:
-                str = 3*sbstr[0]
-        
         return str 
-        
