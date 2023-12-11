b1531798de839af6ffd7f7e0396fdd0addc7f1db
Vratesh Bhardwaj <102753634+CosmicWarriorCW@users.noreply.github.com>
2023-12-11 11:13:51 +0530

    Time: 560 ms (25.40%) | Memory: 14.3 MB (53.17%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
@@ -1,8 +1,15 @@
 class Solution(object):
     def findSpecialInteger(self, arr):
+        result = 0
         array_length = len(arr)
-        quarter_length = array_length // 4
+        quarter = array_length // 4
+        
+        for i in range(array_length):
+            
+            last_occurance = float(array_length - arr[::-1].index(arr[i]) - 1)
+            difference= ((last_occurance - arr.index(arr[i]))+1)
+            
 
-        for i in range(array_length - quarter_length):
-            if arr[i] == arr[i + quarter_length]:
+            if difference > quarter:
                 return arr[i]
+        
