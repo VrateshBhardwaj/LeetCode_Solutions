7f2148142577b6373ab6b4b8be725a4def6ad3b8
Vratesh Bhardwaj <102753634+CosmicWarriorCW@users.noreply.github.com>
2023-12-11 11:09:58 +0530

    Time: 542 ms (30.16%) | Memory: 14.4 MB (42.86%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
@@ -4,10 +4,11 @@ class Solution(object):
         arrayLength = len(arr)
         quarter = arrayLength // 4
         result = 0
+
         for elem in arr:
             firstIndex = float(arr.index(elem)) # finds first index
             lastIndex = arrayLength - arr[::-1].index(elem) - 1 #finds last index
             difference = (lastIndex - firstIndex) 
             
-            if difference + 1 > quarter:
+            if difference  >= quarter:
                 return int(elem)
