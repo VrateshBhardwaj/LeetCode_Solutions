a8b03ef8ea79e0caeffee1a87d7f6c34a9822871
Vratesh Bhardwaj <102753634+CosmicWarriorCW@users.noreply.github.com>
2023-12-16 15:20:27 +0530

    Time: 59 ms (9.95%) | Memory: 13.7 MB (82.80%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,32 @@
 class Solution(object):
     def isAnagram(self, s, t):
-       return sorted(t) == sorted(s)
+
+        anagram = True
+        frequency_str1 = {}
+        frequency_str2 = {}
+        
+        if len(t) != len(s):
+            return False
+
+        for ch in s:
+            count = 0
+            if ch not in frequency_str1:
+                count += 1
+                frequency_str1[ch] = count
+            else:
+                frequency_str1.update({ch:frequency_str1.get(ch)+1})
+
+        for ch in t:
+            count = 0
+            if ch not in frequency_str2:
+                count += 1
+                frequency_str2[ch] = count
+            else:
+                frequency_str2.update({ch:frequency_str2.get(ch)+1})
+
+        for ch in t:
+            if frequency_str1.get(ch) != frequency_str2.get(ch):
+                anagram = False
+                break
+
+        return anagram
